bf5ff7523317932298bf223c7311d6ca721b1715
Ryan Workman <rdavid1099@gmail.com>
2016-11-02 09:23:19 -0600

    Add feature test for user story

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,1 @@
+class SearchesController < ApplicationController
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'items#index'
-
+  get 'search', to: 'searches#index'
   resources :items,  only: [:index, :show]
   resources :orders, only: [:index, :show]
   resources :users,  only: [:index, :show]
